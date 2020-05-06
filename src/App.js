29e70471359ac1cb26fcfae0155d46bd2da2093a
Hugo <apple@Apples-MBP.lan>
2020-05-06 18:42:50 +0800

    finished redux functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
 
 import './App.css';
 
@@ -9,8 +10,11 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SingInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from '../src/redux/user/user.slector'
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -44,13 +48,17 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exact path='/signin' render={() =>
-            this.props.currentUser ? (
-              <Redirect to='/' />
-            ) : (
-              <SingInAndSignUpPage />
-            )
-          }
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                  <SingInAndSignUpPage />
+                )
+            }
           />
         </Switch>
       </div>
@@ -58,8 +66,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
